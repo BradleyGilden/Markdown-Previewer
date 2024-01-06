472eccb33c9c8ee6aaf940dd5be61d5d50cfa2c6
Bradley Gilden <bradleygilden@gmail.com>
2024-01-06 21:24:57 +0200

    base structure

--- md-app/src/App.jsx
+++ md-app/src/App.jsx
@@ -5,7 +5,7 @@ function App() {
       <header className="bg-slate-800 p-6">
         <h1 className=" text-xl sm:text-3xl font-title text-green-500 text-center hover:tracking-wide sm:hover:tracking-[0.5rem] transition-all duration-300">{ title }</h1>
       </header>
-      <div>
+      <div className="flex">
 
       </div>
     </div>
