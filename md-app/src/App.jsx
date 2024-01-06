08a0f48eb5d90493f8607e83bfb49bffda1857e1
Bradley Gilden <bradleygilden@gmail.com>
2024-01-06 19:09:51 +0000

    Sat, Jan 6, 2024, 9:09 PM +02:00

--- md-app/src/App.jsx
+++ md-app/src/App.jsx
@@ -1,9 +1,12 @@
 function App() {
   return (
     <div>
-      <header className="bg-slate-800 p-10">
+      <header className="bg-slate-800 p-6">
         <h1 className="text-3xl font-title text-green-500 text-center">Markdown Preview</h1>
       </header>
+      <div>
+
+      </div>
     </div>
   );
 }
