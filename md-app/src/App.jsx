c29ced4100c5f97977e124f7867d1493bb00782e
Bradley Gilden <bradleygilden@gmail.com>
2024-01-06 19:19:21 +0000

    Sat, Jan 6, 2024, 9:19 PM +02:00

--- md-app/src/App.jsx
+++ md-app/src/App.jsx
@@ -3,7 +3,7 @@ function App() {
   return (
     <div>
       <header className="bg-slate-800 p-6">
-        <h1 className="text-3xl font-title text-green-500 text-center hover:tracking-wide">{ title }</h1>
+        <h1 className="text-3xl font-title text-green-500 text-center hover:tracking-[0.5rem]">{ title }</h1>
       </header>
       <div>
 
